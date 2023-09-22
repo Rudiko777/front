87991c2ab69953881d40b64a5d1499a2b9b72b6c
Rudiko777 <chakryan_rudik@mail.ru>
2023-09-22 11:35:14 +0300

    React

--- src/js/page_1/main/QuickCarInstallation/interactiveBtn/Btn.js
+++ src/js/page_1/main/QuickCarInstallation/interactiveBtn/Btn.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './interBtn.module.css'
 import point from '../../../../../images/page_1/main/pointBlack.svg'
 
 export const Btn = ({title})=>{
+    const[stylePoint, setStylePoint] = useState(false)
+    const[styleListBtn, setStyleListBtn] = useState(false)
+
+    const handlePoint = ()=>{
+        setStylePoint(!stylePoint);
+    }
+
+    const handleListBtn = ()=>{
+        setStyleListBtn(!styleListBtn)
+    }
+
     return(
         <>
-            <button className={styles.Btn}>
-                <div>
-                    <p>{title}</p>
-                    <img className={styles.pointImg} src={point}/>
-                </div>
-            </button>
+            <div className={styles.GeneralBox}>
+                <button className={styles.Btn}>
+                    <div>
+                        <p>{title}</p>
+                        <img className={stylePoint ? styles.pointImgActive : styles.pointImg} src={point} onClick={handlePoint}/>
+                    </div>
+                </button>
+                {stylePoint ?
+                    <>
+                        <div className={styles.btnInsideBox}>
+                            <ul className={styles.btnList}>
+                                <li className={styles.btnListItem}>
+                                    <a className={styles.btnListItemLink} href={"#"}>
+                                        A220
+                                    </a>
+                                </li>
+                                <li className={styles.btnListItem}>
+                                    <a className={styles.btnListItemLink} href={"#"}>
+                                        B567
+                                    </a>
+                                </li>
+                                <li className={styles.btnListItem}>
+                                    <a className={styles.btnListItemLink} href={"#"}>
+                                        V699
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </> :
+                    null
+                }
+            </div>
         </>
     )
 }
--- src/js/page_1/main/QuickCarInstallation/scale/numOfScale/numOfScale.js
+++ src/js/page_1/main/QuickCarInstallation/scale/numOfScale/numOfScale.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useState} from "react";
 import styles from './numOfScale.module.css'
 
 export const NumOfScale = ({title})=>{
+    const[valueScale, setValueScale] = useState(false)
+
+    useEffect(()=>{
+        if ({title} === "0"){
+            setValueScale(true);
+        }
+    }, [title])
+
     return(
-        <div className={styles.numberBox}>
+        <div className={valueScale ? styles.numberBoxNull : styles.numberBox}>
             {title}
         </div>
     )
